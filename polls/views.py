5c1a61c59af3d194285c1104f32b8a275d2547f3
whuan123 <wh_sina@126.com>
2018-12-19 23:25:29 +0800

    类化视图

--- polls/views.py
+++ polls/views.py
@@ -5,13 +5,14 @@ from django.shortcuts import render, get_object_or_404
 from django.shortcuts import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+from django.views import generic
 
 from .models import Question, Choice
 from django.template import loader
 from django.http import Http404
 
 
-
+"""
 def index(request):
 
     latest_question_list = Question.objects.order_by('-publish_date')[:5]
@@ -52,6 +53,38 @@ def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
 
     return render(request, 'polls/results.html', {'question': question})
+
+"""
+
+class IndexView(generic.ListView):
+
+    template_name = 'polls/index.html'
+
+    context_object_name = 'latest_question_list'
+
+
+    def get_queryset(self):
+
+    #返回最近发布的5个问卷.
+
+        return Question.objects.order_by('-publish_date')[:5]
+
+
+
+class DetailView(generic.DetailView):
+
+    model = Question
+
+    template_name = 'polls/detail.html'
+
+
+
+class ResultsView(generic.DetailView):
+
+    model = Question
+
+    template_name ='polls/results.html'
+
 
 
 def vote(request, question_id):
