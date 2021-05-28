d5bdd95c41564fab7818b56dcfcb2217c00c1105
whuan123 <wh_sina@126.com>
2021-05-28 22:20:54 +0800

    添加models

--- polls/models.py
+++ polls/models.py
@@ -6,7 +6,6 @@ from django.utils import timezone
 # Create your models here.
 
 
-
 class Question(models.Model):
 
     question_text = models.CharField(max_length=200)
@@ -16,11 +15,9 @@ class Question(models.Model):
 
         return self.question_text
 
-
     def is_published_recently(self):
 
         return self.publish_date >= timezone.now() - datetime.timedelta(days=1)
-
 
 
 class Choice(models.Model):
@@ -32,6 +29,3 @@ class Choice(models.Model):
     def __str__(self):
 
         return self.choice_text
-
-
-
